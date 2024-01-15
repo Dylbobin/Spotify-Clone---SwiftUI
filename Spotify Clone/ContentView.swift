932667b2cb8b2a699f57446a6c8398533d62075d
Dylbobin <dylansilva22@yahoo.com>
2024-01-15 12:35:05 -0800

    Initial Commit

---
+++ Spotify Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spotify Clone
+//
+//  Created by Dylan Silva on 1/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spotify Clone/Spotify_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_CloneApp.swift
+//  Spotify Clone
+//
+//  Created by Dylan Silva on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
